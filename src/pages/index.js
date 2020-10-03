d6f4c47775808a35f1c016a2327fed938367112e
Jasmin Merchant <jasmin@Jasmins-MacBook-Air.local>
2020-10-03 23:42:30 +0530

    Added links to blog posts on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,37 @@
 import React from "react"
+import { graphql } from "gatsby"
+import PostLink from "../components/post-link"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+
+  return <div>
+    Check out all the posts! <br /> <br />
+    {Posts}</div>
 }
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
